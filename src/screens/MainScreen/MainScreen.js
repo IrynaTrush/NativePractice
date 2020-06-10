755e542eaf2ea8ca1b083ba11a8bf5410b1612c3
Iryna Trush <itrush@jappware.com>
2020-06-10 10:58:11 +0300

    first commit

---
+++ src/screens/MainScreen/MainScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, View, FlatList} from 'react-native';
+import { AddTodo } from '../../components/AddTodo.js/AddTodo';
+import { Todo } from '../../components/Todo/Todo';
+
+export const MainScreen = ({ addTodo, todos, removeTodo })=> {
+    return (
+        <View>
+            <AddTodo onSubmit={addTodo}/>
+            <FlatList 
+                keyExtractor={item => item.id}
+                data={todos}
+                renderItem={({item}) => <Todo todo={item} 
+                onRemove={removeTodo}/>}
+                />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
