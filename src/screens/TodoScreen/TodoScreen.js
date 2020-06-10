270256d4643a515a4833eb15fc5a02710c1bab5e
Iryna Trush <itrush@Irynas-iMac.local>
2020-06-10 14:43:23 +0300

    styled buttons

--- src/screens/TodoScreen/TodoScreen.js
+++ src/screens/TodoScreen/TodoScreen.js
@@ -8,9 +8,22 @@ export const TodoScreen = ({goBack, todo}) => {
             <Text>
                {todo.title}
             </Text>
-            <Button title="Back" onPress={goBack} />
+            <View style={styles.buttons}>
+                <View style={styles.button}><Button title="Back" onPress={goBack} /></View>
+                <View style={styles.button}><Button title="Delete" color="#ff0000" onPress={() => console.log('aaa')} /></View>
+
+              
+            </View>
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    buttons: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+    },
+    button: {
+        width: '40%',
+    }
+})
