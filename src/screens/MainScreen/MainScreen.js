6f810a9e8fe62e5ea31b5bf8d87240cd464cff67
Iryna Trush <itrush@Irynas-iMac.local>
2020-06-10 16:46:25 +0300

    tried to add image

--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, FlatList} from 'react-native';
+import { StyleSheet, View, FlatList, Text} from 'react-native';
 import { AddTodo } from '../../components/AddTodo.js/AddTodo';
 import { Todo } from '../../components/Todo/Todo';
 
-export const MainScreen = ({ addTodo, todos, removeTodo, openTodo })=> {
+export const MainScreen = ({ addTodo, todos, removeTodo, openTodo, Image })=> {
+    let content = (<FlatList 
+    keyExtractor={item => item.id}
+    data={todos}
+    renderItem={({item}) => <Todo todo={item} 
+    onRemove={removeTodo}
+    onOpen={openTodo}/>}
+    />)
+
+    if (todos.length === 0) { 
+        content = ( <View style={styles.imgWrap}>
+           
+           <Text>no items yet</Text>
+        </View>)
+    }
     return (
         <View>
             <AddTodo onSubmit={addTodo}/>
-            <FlatList 
-                keyExtractor={item => item.id}
-                data={todos}
-                renderItem={({item}) => <Todo todo={item} 
-                onRemove={removeTodo}
-                onOpen={openTodo}/>}
-                />
+            {content}
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imgWrap: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: 300
+    }
+})
